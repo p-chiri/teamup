f2dc0d4ac4b77dd650bf511216609144fa49ea21
Arnold kipchirchir <p-chiri>
2023-01-11 12:50:01 +0300

    Intial Commit

---
+++ config/importmap.rb
@@ -0,0 +1,8 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "flowbite", to: "https://unpkg.com/flowbite@1.6.0/dist/flowbite.turbo.js"
